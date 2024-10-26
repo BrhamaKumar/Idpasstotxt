b81bd4dfba54e360e5e6a5d85d1832a4fa4ed807
Bkphere <163238356+Bkphere@users.noreply.github.com>
2024-10-26 11:32:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7873431576:AAF6emAkfteDXkp6PMMees0y_J_iqvkEI88")
     API_ID = int(os.environ["API_ID", 28745135]
     API_HASH = os.environ["API_HASH", "123e77214315877fe406e25f35afd23d"]
-    AUTH_USERS = "6488911325"""
+    AUTH_USERS = "6750546542"""
 # jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/Stubborn1223 ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
